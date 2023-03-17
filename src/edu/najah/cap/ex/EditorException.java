795e75117d96ff7f12943c049d289174771e1cc4
LmaAl-Johare1 <lmaref.j@gmail.com>
2023-03-17 23:50:40 +0200

    removed unused parameter

--- src/edu/najah/cap/ex/EditorException.java
+++ src/edu/najah/cap/ex/EditorException.java
@@ -5,11 +5,8 @@ import java.io.Serial;
 public class EditorException extends Exception {
     @Serial
     private static final long serialVersionUID = 1L;
-    public EditorException(String message) {
-        this(message, 0);
-    }
 
-    public EditorException(String message, int errorCode) {
+    public EditorException(String message) {
         super(message);
     }
 
